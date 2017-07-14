89c5fe1d010f0d6705d60efc454e07184f227180
aawesh <shrestha.aawesh@gmail.com>
2017-07-14 14:39:56 -0500

    -made app to support multiple version <23 >23

--- app/src/main/java/viewing/com/viewing/MainActivity.java
+++ app/src/main/java/viewing/com/viewing/MainActivity.java
@@ -1,17 +1,24 @@
 package viewing.com.viewing;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.io.File;
@@ -36,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private EditText editText;
     private TextView textView;
 
+    private RelativeLayout rLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,11 +55,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         editText = (EditText)findViewById(R.id.editText);
         textView = (TextView) findViewById(R.id.textView);
 
+        rLayout = (RelativeLayout)findViewById(R.id.mainLayout);
+
 
         button.setOnClickListener(this);
+        rLayout.setOnClickListener(this);
+
+        button.setClickable(false);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        System.out.println("Build.VERSION.SDK_INT >= 23 = " + Build.VERSION.SDK_INT);
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ) {
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 12);
+
+                return;
+            }
+        }else{
+            button.setClickable(true);
+        }
     }
 
 
@@ -63,7 +96,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        System.out.println("Reached here======================= ");
         // In this example, alpha is calculated as t / (t + dT),
         // where t is the low-pass filter's time-constant and
         // dT is the event delivery rate.
@@ -95,6 +127,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onClick(View v) {
+
+        InputMethodManager imm = (InputMethodManager)getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
         switch (v.getId()){
             case R.id.button1:
                 if(!start){
@@ -141,6 +178,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             fOut.close();
         } catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 12: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0   && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    button.setClickable(true);
+
+                } else {
+
+                    button.setClickable(false);
+
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
     }
 }
